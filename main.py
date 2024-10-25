87a6e89a41b361880d5b623b21dfab6141904e9f
pyralasis <pyralas29@gmail.com>
2024-10-25 13:55:34 -0400

    moved data getting to the dataset

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from filereader import *
+from trainingmodels import *
 
 
 def main():
-    data = getFileFromUrl('https://raw.githubusercontent.com/jerbarnes/semeval22_structured_sentiment/refs/heads/master/data/opener_en/dev.json')
-    myDataPoints = []
-    for item in data:
-        myDataPoints.append(dataPoint(item))
+    model = NeuralNetwork().to(device)
+    print(model)
     
 if __name__=="__main__":
     main()
