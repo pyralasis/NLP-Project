f3cb45b2329f747b0093fb65ed35ad4ef93cfb48
pyralasis <pyralas29@gmail.com>
2024-10-25 13:55:21 -0400

    Added some node members

--- StructuredSentimentGraph.py
+++ StructuredSentimentGraph.py
@@ -1,12 +1,22 @@
 class StructuredSentimentGraph:
     def __init__(self):
-        ...
+        self.expression_head = None
+        self.holder_head = None
+        self.target_head = None
 
 class HolderNode:
-    ...
+    def __init__(self):
+        self.head = None
+        self.next = None
 
 class ExpressionNode:
-    ...
+    def __init__(self):
+        self.head = None
+        self.next = None
+        self.holder_head = None
+        self.holder_head = None
 
 class TargetNode:
-    ...
+    def __init__(self):
+        self.head = None
+        self.next = None
