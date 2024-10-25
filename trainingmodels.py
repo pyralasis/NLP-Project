53a48607e1009e3408e8ee4d150d05276329ebc1
pyralasis <pyralas29@gmail.com>
2024-10-25 13:55:46 -0400

    init model

---
+++ trainingmodels.py
@@ -0,0 +1,52 @@
+import torch
+from torch.ao import nn
+from torchtext.models import XLMR_BASE_ENCODER
+
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.encoder = XLMR_BASE_ENCODER
+
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(768, 2),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+    def train(dataloader, model, loss_fn, optimizer):
+        size = len(dataloader.dataset)
+        model.train()
+        for batch, (X, y) in enumerate(dataloader):
+            X, y = X.to(device), y.to(device)
+
+            # Compute prediction error
+            pred = model(X)
+            loss = loss_fn(pred, y)
+
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if batch % 100 == 0:
+                loss, current = loss.item(), (batch + 1) * len(X)
+                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+model = NeuralNetwork().to(device)
+print(model)
